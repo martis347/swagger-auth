6a3bc30db8c608a7393c93222b37a694573a8762
Martynas Kanapinskas <m.kanapinskas@hyarchis.com>
2022-07-20 09:42:16 +0300

    Updated zip/xpi with latest changes

---
+++ extension/sign-firefox.sh
@@ -0,0 +1,2 @@
+# Key should be generated in: https://addons.mozilla.org/en-US/developers/addon/api/key/
+web-ext sign --api-key=user: --api-secret=
--- sign.sh
+++
@@ -1,1 +0,0 @@
-web-ext sign --api-key=user:16263293:52 --api-secret=b758bebf3cf919ca8af7be08750f7e93272e92e9276c445ff09a205aa3da40da
